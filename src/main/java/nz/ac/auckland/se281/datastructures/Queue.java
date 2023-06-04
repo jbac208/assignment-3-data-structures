885f47f819d386159377928adfe3840b3a587508
jbac208 <jbac208@aucklanduni.ac.nz>
2023-06-04 23:43:33 +1200

    stack and queue ADT interfaces done

---
+++ src/main/java/nz/ac/auckland/se281/datastructures/Queue.java
@@ -0,0 +1,14 @@
+package nz.ac.auckland.se281.datastructures;
+
+public interface Queue<T> {
+
+  public int size();
+
+  public boolean isEmpty();
+
+  public T front();
+
+  public void enqueue(T element);
+
+  public T dqueue();
+}
